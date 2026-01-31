a9814ecffff09fc0aa8efa77b2eed3abddc2d95e
Yongheng Liu <161477977+huahai27@users.noreply.github.com>
2026-01-31 21:55:15 +0800

    done with proj1

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,112 @@
+package deque;
+
+public class ArrayDeque<T> implements Deque<T> {
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[100];
+        size = 0;
+        nextFirst = 20;
+        nextLast = 21;
+    }
+
+    public void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        int start = nextFirst + 1;
+        int end = nextLast - 1;
+        nextFirst = capacity / 4;
+        nextLast = nextLast + 1;
+        int cursor = nextFirst;
+
+        while (start != end) {
+            if (start > size - 1) start = 0;
+            a[cursor] = items[start];
+            cursor++;
+            start++;
+        }
+
+        items = a;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        if (size == items.length) resize(size * 2);
+        items[nextFirst] = item;
+        nextFirst--;
+        if (nextFirst < 0) nextFirst = items.length - 1;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        if (size == items.length) resize(size * 2);
+        items[nextLast] = item;
+        nextLast++;
+        if (nextLast > items.length - 1) nextLast = 0;
+        size++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        int cursor = nextFirst + 1;
+
+        while (cursor != nextLast) {
+            if (cursor == items.length) cursor = 0;
+
+            System.out.print(items[cursor] + " ");
+            cursor++;
+        }
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0) return null;
+        T remove = items[++nextFirst];
+        if (nextFirst > items.length - 1) nextFirst = 0;
+        size--;
+        return remove;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0) return null;
+        T remove = items[--nextLast];
+        if (nextLast < 0) nextLast = items.length - 1;
+        size--;
+        return remove;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size) {
+            throw new ArrayIndexOutOfBoundsException(
+                    String.format("索引 %d 超出数组大小 %d", index, size)
+            );
+        } else {
+            int cnter = 0;
+            int cursor = nextFirst + 1;
+
+            while (cnter != index) {
+                if (cursor > items.length - 1) cursor = 0;
+                cnter++;
+                cursor++;
+            }
+
+            T item = items[cursor];
+            return item;
+        }
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,136 @@
+package deque;
+
+public class LinkedListDeque<T> implements Deque<T>{
+    private class Node {
+        public T item;
+        public Node prev;
+        public Node next;
+
+        public Node(T i, Node p, Node n) {
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+
+    private Node sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+    public LinkedListDeque(T x) {
+        sentinel = new Node(null, null, null);
+        sentinel.next = new Node(x, sentinel, sentinel);
+        sentinel.prev = sentinel.next;
+        size = 1;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        Node preFirst = sentinel.next;
+        sentinel.next = new Node(item, sentinel, preFirst);
+        preFirst.prev = sentinel.next;
+        size += 1;
+    }
+
+    @Override
+    public void addLast(T item) {
+        Node preLast = sentinel.prev;
+        sentinel.prev = new Node(item, preLast, sentinel);
+        preLast.next = sentinel.prev;
+        size += 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        Node n = sentinel.next;
+        while (n.next != sentinel.next) {
+            System.out.print(n.item + " ");
+            n = n.next;
+        }
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0) return null;
+        Node remove = sentinel.next;
+        T item = remove.item;
+        Node newFirst = remove.next;
+        sentinel.next = newFirst;
+        newFirst.prev = sentinel;
+        remove.item = null;
+        remove.prev = null;
+        remove.next = null;
+        size -= 1;
+        return item;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0) return null;
+        Node remove = sentinel.prev;
+        T item = remove.item;
+        Node newLast = remove.prev;
+        sentinel.prev = newLast;
+        newLast.next = sentinel;
+        remove.item = null;
+        remove.prev = null;
+        remove.next = null;
+        size -= 1;
+        return item;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size) {
+            throw new ArrayIndexOutOfBoundsException(
+                    String.format("索引 %d 超出数组大小 %d", index, size)
+            );
+        } else {
+            Node n = sentinel.next;
+            int cnter = 0;
+
+            while (cnter != index) {
+                n = n.next;
+                cnter++;
+            }
+
+            T item = n.item;
+            return item;
+        }
+    }
+
+    private T getRecursiveHelper(Node l, int index) {
+        if (index == 0) {
+            return l.item;
+        } else {
+            return getRecursiveHelper(l.next, index - 1);
+        }
+    }
+
+    public T getRecursive(int index) {
+        if (index >= size) {
+            throw new ArrayIndexOutOfBoundsException(
+                    String.format("索引 %d 超出数组大小 %d", index, size)
+            );
+        } else {
+            Node n = sentinel.next;
+            return getRecursiveHelper(n, index);
+        }
+    }
+}
